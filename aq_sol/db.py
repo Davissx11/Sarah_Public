b43a4a383bb28ea98e1b63b0485537ddc2038426
John Hanley <jhanley634@users.noreply.github.com>
2025-08-28 13:52:40 -0700

    Now "create" simply returns a table_name

--- aq_sol/db.py
+++ aq_sol/db.py
@@ -1,8 +1,7 @@
 from pathlib import Path
 
 import numpy as np
-from sqlalchemy import Engine, MetaData, Table, create_engine, func, select, text
-from sqlalchemy.orm import sessionmaker
+from sqlalchemy import Engine, create_engine, text
 
 from aq_sol.eda import TMP, get_solubility_df
 
@@ -10,10 +9,10 @@ DB_FILE = TMP / "aq_sol.sqlite"
 
 
 def get_engine(db_file: Path = DB_FILE) -> Engine:
-    return create_engine(f"sqlite:///{db_file}")
+    return create_engine(f"sqlite:///{db_file}", isolation_level="AUTOCOMMIT")
 
 
-def create_aq_sol_table(table_name: str = "aq_sol") -> int:
+def create_aq_sol_table(table_name: str = "aq_sol") -> str:
     with get_engine().connect() as conn:
         drop = text(f"DROP TABLE  IF EXISTS  {table_name}")
         conn.execute(drop)
@@ -22,12 +21,4 @@ def create_aq_sol_table(table_name: str = "aq_sol") -> int:
         df["Ocurrences"] = df.Ocurrences.astype(np.int32)
 
         df.to_sql(table_name, conn, index=False)
-
-        meta = MetaData()
-        meta.reflect(bind=conn)
-        table = Table(table_name, meta, autoload_with=conn)
-        with sessionmaker(bind=conn)() as sess:
-            query = select(func.count()).select_from(table)
-            num_rows = sess.execute(query).scalar()
-            assert isinstance(num_rows, int)
-            return num_rows
+        return table_name
--- tests/aq_sol/db_test.py
+++ tests/aq_sol/db_test.py
@@ -1,8 +1,22 @@
 import unittest
 
-from aq_sol.db import create_aq_sol_table
+from sqlalchemy import MetaData, Table, func, select
+from sqlalchemy.orm import sessionmaker
+
+from aq_sol.db import create_aq_sol_table, get_engine
 
 
 class DbTest(unittest.TestCase):
+
     def test_count(self) -> None:
-        self.assertEqual(9_982, create_aq_sol_table())
+        table_name = create_aq_sol_table()
+
+        with sessionmaker(bind=get_engine())() as sess:
+            meta = MetaData()
+            meta.reflect(bind=sess.get_bind())
+            table = Table(table_name, meta, autoload_with=sess.get_bind())
+
+            query = select(func.count()).select_from(table)
+            num_rows = sess.execute(query).scalar()
+            assert isinstance(num_rows, int)
+            self.assertEqual(9_982, num_rows)
