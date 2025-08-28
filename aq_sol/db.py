20c53c4e4f6a2bbaa60b7b5d3fc6d460dee11ac4
John Hanley <jhanley634@users.noreply.github.com>
2025-08-27 20:19:46 -0700

    Return the count of INSERTed rows

--- aq_sol/db.py
+++ aq_sol/db.py
@@ -1,7 +1,8 @@
 from pathlib import Path
 
 import numpy as np
-from sqlalchemy import Engine, create_engine, text
+from sqlalchemy import Engine, MetaData, Table, create_engine, func, select, text
+from sqlalchemy.orm import sessionmaker
 
 from aq_sol.eda import TMP, get_solubility_df
 
@@ -12,7 +13,7 @@ def get_engine(db_file: Path = DB_FILE) -> Engine:
     return create_engine(f"sqlite:///{db_file}")
 
 
-def create_aq_sol_table(table_name: str = "aq_sol") -> None:
+def create_aq_sol_table(table_name: str = "aq_sol") -> int:
     with get_engine().connect() as conn:
         drop = text(f"DROP TABLE  IF EXISTS  {table_name}")
         conn.execute(drop)
@@ -21,3 +22,12 @@ def create_aq_sol_table(table_name: str = "aq_sol") -> None:
         df["Ocurrences"] = df.Ocurrences.astype(np.int32)
 
         df.to_sql(table_name, conn, index=False)
+
+        meta = MetaData()
+        meta.reflect(bind=conn)
+        table = Table(table_name, meta, autoload_with=conn)
+        with sessionmaker(bind=conn)() as sess:
+            query = select(func.count()).select_from(table)
+            num_rows = sess.execute(query).scalar()
+            assert isinstance(num_rows, int)
+            return num_rows
--- tests/aq_sol/db_test.py
+++ tests/aq_sol/db_test.py
@@ -5,4 +5,4 @@ from aq_sol.db import create_aq_sol_table
 
 class DbTest(unittest.TestCase):
     def test_count(self) -> None:
-        create_aq_sol_table()
+        self.assertEqual(9_982, create_aq_sol_table())
