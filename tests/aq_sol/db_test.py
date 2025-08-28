f4ab0ad764fac6de402f795c8132844cd24566c5
John Hanley <jhanley634@users.noreply.github.com>
2025-08-28 14:03:03 -0700

    Add get_session_meta() helper

--- tests/aq_sol/db_test.py
+++ tests/aq_sol/db_test.py
@@ -1,9 +1,23 @@
 import unittest
+from collections.abc import Generator
+from contextlib import contextmanager
 
 from sqlalchemy import MetaData, Table, func, select
-from sqlalchemy.orm import sessionmaker
+from sqlalchemy.orm import Session, sessionmaker
 
 from aq_sol.db import create_aq_sol_table, get_engine
+
+
+@contextmanager
+def get_session_meta() -> Generator[tuple[Session, MetaData], None, None]:
+    engine = get_engine()
+    session = sessionmaker(bind=engine)()
+    meta = MetaData()
+    meta.reflect(bind=engine)
+    try:
+        yield session, meta
+    finally:
+        session.close()
 
 
 class DbTest(unittest.TestCase):
@@ -11,12 +25,9 @@ class DbTest(unittest.TestCase):
     def test_count(self) -> None:
         table_name = create_aq_sol_table()
 
-        with sessionmaker(bind=get_engine())() as sess:
-            meta = MetaData()
-            meta.reflect(bind=sess.get_bind())
+        with get_session_meta() as (sess, meta):
             table = Table(table_name, meta, autoload_with=sess.get_bind())
 
             query = select(func.count()).select_from(table)
             num_rows = sess.execute(query).scalar()
-            assert isinstance(num_rows, int)
             self.assertEqual(9_982, num_rows)
