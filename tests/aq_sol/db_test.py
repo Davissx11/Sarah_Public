1411b339f0dd9e4f6ac305d1351578823206dc6d
John Hanley <jhanley634@users.noreply.github.com>
2025-08-28 14:16:15 -0700

    Add a raw SQL query

--- tests/aq_sol/db_test.py
+++ tests/aq_sol/db_test.py
@@ -2,7 +2,7 @@ import unittest
 from collections.abc import Generator
 from contextlib import contextmanager
 
-from sqlalchemy import MetaData, Table, func, select
+from sqlalchemy import MetaData, Table, func, select, text
 from sqlalchemy.orm import Session, sessionmaker
 
 from aq_sol.db import create_aq_sol_table, get_engine
@@ -22,12 +22,21 @@ def get_session_meta() -> Generator[tuple[Session, MetaData], None, None]:
 
 class DbTest(unittest.TestCase):
 
-    def test_count(self) -> None:
+    def test_count_rows(self) -> None:
+
         table_name = create_aq_sol_table()
 
+        # raw SQL
+        with get_engine().connect() as conn:
+            sel = text(f"SELECT COUNT(*) FROM {table_name}")
+            (row,) = conn.execute(sel)
+            (count,) = row
+            self.assertEqual(9_982, count)
+
+        # ORM
         with get_session_meta() as (sess, meta):
             table = Table(table_name, meta, autoload_with=sess.get_bind())
 
             query = select(func.count()).select_from(table)
-            num_rows = sess.execute(query).scalar()
-            self.assertEqual(9_982, num_rows)
+            count = sess.execute(query).scalar()
+            self.assertEqual(9_982, count)
