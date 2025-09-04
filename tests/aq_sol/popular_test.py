a739b58d26d37dd43e3be6aa85547d91c0039daa
John Hanley <jhanley634@users.noreply.github.com>
2025-09-03 18:30:21 -0700

    Add _to_snake_case()

--- tests/aq_sol/popular_test.py
+++ tests/aq_sol/popular_test.py
@@ -1,8 +1,10 @@
 import re
 import unittest
 
-from aq_sol.eda import get_solubility_df
-from aq_sol.popular import popular
+import pandas as pd
+
+from aq_sol.eda import DESKTOP, get_solubility_df
+from aq_sol.popular import PopCache, popular
 
 _comma_first_phrase = re.compile(r"(.+) *,")
 _sem_last_phrase = re.compile(r".*; *(.+)")
@@ -27,3 +29,16 @@ class PopularTest(unittest.TestCase):
 
     def test_popular(self) -> None:
         self.assertGreater(popular("Potassium_chloride"), 19_000)  # 19_710
+
+    def test_add_names(self, max_width: int = 40) -> None:
+        pc = PopCache()
+        pc.DB_FILE.unlink()
+        df = get_solubility_df().sort_values(by="name")
+        df["name"] = df.name.apply(lambda x: str(x)[:max_width])
+        df = df[["name", "mol_wt", "mol_log_p", "balaban_j", "solubility"]]
+        df.to_csv(DESKTOP / "sol.csv", index=False)
+
+        # For now, focus on just the rows whose .name starts with capital "A".
+        assert isinstance(df.name, pd.Series)
+        df_a = df[df.name.str.startswith("A")]
+        self.assertEqual(37, len(df_a))
