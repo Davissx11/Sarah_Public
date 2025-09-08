1302403137454bb6c95e22c2172918fbca874c5c
John Hanley <jhanley634@users.noreply.github.com>
2025-09-07 21:00:21 -0700

    Add a CSV --> DB table loader

--- aq_sol/snapshot.py
+++ aq_sol/snapshot.py
@@ -1,5 +1,10 @@
 #! /usr/bin/env YDATA_SUPPRESS_BANNER=1 python
 
+"""
+To refresh RDBMS from *.csv files:
+    $ rm /tmp/wiki_cache.sqlite; aq_sol/snapshot.py
+"""
+
 from pathlib import Path
 
 import pandas as pd
@@ -21,5 +26,22 @@ def write_wiki_snapshot() -> None:
         df.to_csv(OUT / f"{table}.csv", index=False)
 
 
+def load_tables_if_empty() -> None:
+    pc = PopCache()
+    select = "SELECT COUNT(*)  FROM pview"
+    with pc.get_session() as sess:
+        row = sess.execute(text(select)).first()
+        assert row
+    (n,) = row
+    if int(n) == 0:
+        df_c = pd.read_csv(OUT / "cname.csv")
+        df_c["cname"] = df_c.cname.replace({None: ""})
+        df_c.to_sql("cname", pc.conn, if_exists="append", index=False)
+
+        df_pv = pd.read_csv(OUT / "pview.csv")
+        df_pv.to_sql("pview", pc.conn, if_exists="append", index=False)
+
+
 if __name__ == "__main__":
+    load_tables_if_empty()
     write_wiki_snapshot()
